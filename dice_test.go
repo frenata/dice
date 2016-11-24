effd19a75448dde893a9fdc7823e432b4fbab153
Andrew Nichols <danweasel@gmail.com>
2016-11-24 16:29:40 +0700

    extractable Int() int function added and to interface and implemented; basic tests added

--- dice_test.go
+++ dice_test.go
@@ -36,3 +36,17 @@ func TestRoll(t *testing.T) {
 		t.Fatalf("%s is not a VsResult", roll)
 	}
 }
+
+func TestResultInt(t *testing.T) {
+	roll := "6d1" // aka 6
+	res, _, _ := Roll(roll)
+	if res.Int() != 6 {
+		t.Fatalf("%s does not evaluate to 6", roll)
+	}
+
+	roll = "10d10v1" // aka 6
+	res, _, _ = Roll(roll)
+	if res.Int() != 10 {
+		t.Fatalf("%s fails to always roll at least 1", roll)
+	}
+}
